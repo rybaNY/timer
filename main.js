e72ac721a1890c774ec587a942a90f3c814dba1d
Rafal <ryba_ny@hotmail.com>
2018-12-06 21:22:55 -0500

    my first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let timer = () => {
+    number = 0;
+    return go = () => {
+        document.body.textContent = `${number++} sekund`;
+        document.body.style.fontSize = '60px';
+        document.body.style.margin = '30px';
+    }
+}
+timer();
+
+setInterval(go, 1000);
